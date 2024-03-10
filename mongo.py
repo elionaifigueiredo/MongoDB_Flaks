27c32f388ab6185a8210a05eb1c8a101931508e5
Elionai Figueiredo <elionaifigueiredo@gmail.com>
2024-03-09 23:55:46 -0300

    aplicação todo com Flask e MongoDB

---
+++ mongo.py
@@ -0,0 +1,35 @@
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://python:q3nf4QM139r9Vsoq@cluster0.as00imp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+
+# inserção de dados
+    
+db = client['loja_farmacia']
+collection = db['farmacia']
+
+farmacia = [
+    {
+    "codigo":"1",
+    "nome":"Dipirona",
+    "valor":"3.00"
+    },
+    {
+    "codigo":"2",
+    "nome":"Amoxilina",
+    "valor":"30.00"
+    },
+]
+
+collection.insert_many(farmacia)
+print(collection.find_one())
